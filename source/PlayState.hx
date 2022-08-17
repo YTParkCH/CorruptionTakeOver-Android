597fef8b3478ad208770d49ac934b0a122652af2
YTParkCH <109469670+YTParkCH@users.noreply.github.com>
2022-08-17 10:35:23 +0900

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -385,6 +385,8 @@ class PlayState extends MusicBeatState
 					curStage = 'schoolEvil';
 				case 'ugh' | 'guns' | 'stress':
 					curStage = 'tank';
+                                case 'loaded' | 'unable' | 'one-shot
+                                        curStage = picod2
 				default:
 					curStage = 'stage';
 			}
